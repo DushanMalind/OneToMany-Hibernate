e2eaf7689a4470555c3f463082d733fefb0aa24b
DushanMalind <dushanmalinda010@gmail.com>
2023-03-05 17:48:01 +0530

    ok

---
+++ OneToManyRe/src/lk/ijse/OneToMany/entity/OrderDeatils.java
@@ -0,0 +1,8 @@
+package lk.ijse.OneToMany.entity;
+
+public class OrderDeatils {
+    private String code;
+    private String itemCode;
+    private double unitPrice;
+    private int qty;
+}
--- OneToManyRe/src/lk/ijse/OneToMany/entity/Orders.java
+++ OneToManyRe/src/lk/ijse/OneToMany/entity/Orders.java
@@ -1,5 +1,47 @@
 package lk.ijse.OneToMany.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Orders")
 public class Orders {
 
+    private String orderId;
+    private LocalDate date;
+    private String customerId;
+
+    public Orders() {
+    }
+
+    public Orders(String orderId, String customerId) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "orderId='" + orderId + '\'' +
+                ", date=" + date +
+                ", customerId='" + customerId + '\'' +
+                '}';
+    }
 }
